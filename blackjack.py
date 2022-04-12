202be9bb25f1412b973f2a055524bddad6b7790b
nixbytes <nixbytes@users.noreply.github.com>
2022-04-11 21:59:12 -0400

    add main file

---
+++ blackjack.py
@@ -0,0 +1,11 @@
+"""Blackjack, by Al Sweigart al@inventwithpython.com
+The classic card game also known as 21. (This version doesn't have
+splitting or insurance.)
+More info at: https://en.wikipedia.org/wiki/Blackjack
+View this code at https://nostarch.com/big-book-small-python-projects
+Tags: large, game, card game"""
+
+import sys, random
+
+# Set up the constants:
+HEARTS = chr(9829)  # Character 9829 is '♥'.
